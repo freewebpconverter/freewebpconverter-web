51021b31cae06c4af6a9d4f34795f0336924caab
Yasin KALKAN <yasinburakkalkan@gmail.com>
2023-02-06 19:34:09 +0300

    [fix] Problem is fixed.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   // i18n,
   experimental: {
     fontLoaders: [
